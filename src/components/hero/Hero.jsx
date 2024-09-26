4edb915f952eb461e0f0809b0f1094c06cbdb54b
Мөнхсаруул Ганбат <24LP9058@Users-iMac-2.local>
2024-09-26 12:59:59 +0800

    baraag Baraa bolgoson

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,15 +1,13 @@
-import Sort from "../Angilal/Sort";
 import ProductCard from "../product-card/ProductCard";
 
 const Hero = () => {
   return (
     <div className="flex flex-col gap-6 items-center mt-7">
       <div className="flex w-full justify-center">
-        <button className="btn w-max bg-green-400">бараа нэмэх</button>
+        <button className="btn w-max bg-green-400">Бараа нэмэх</button>
       </div>
       <div className="flex w-[1640px] justify-center flex-wrap gap-7">
         <ProductCard />
-
       </div>
     </div>
   );
